01681050c574372cc5abe4ae506a23afddf9d393
Roman <romchikus1987@gmail.com>
2024-12-20 07:49:53 +0200

    used StorageSlot library based on EIP-1967

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -3,19 +3,24 @@ pragma solidity ^0.8.28;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
-
+import './StorageSlot.sol';
 //EOA -> Proxy-> Logic1
 //            -> Logic2
 contract Proxy {
-    uint x = 0;
-    address implementation;
+    //uint x = 0;
+    //address implementation;
+
+    //we use StorageSlot because test would fail at line 38. It's because it read storage slot "0x0" in proxy, and it is occupied by
+    // address implementation variable. But now with StorageSlot we use a deterministic but random-looking slot for the
+    // implementation address
+    //keccak256('impl') generates a unique slot number very far from 0
 
     function changeImplementation(address _implementation) external {
-        implementation = _implementation;
+        StorageSlot.getAddressSlot(keccak256('impl')).value = _implementation;
     }
 
     fallback() external {
-        (bool success, ) = implementation.delegatecall(msg.data);
+        (bool success, ) = StorageSlot.getAddressSlot(keccak256('impl')).value.delegatecall(msg.data);
         require(success);
     }    
      
